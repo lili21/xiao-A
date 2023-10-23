46d488288ca745c65b00996bdb5a5f4ecf12df9a
lili.21 <lili.21@bytedance.com>
2023-10-23 15:34:36 +0800

    chore: api host

--- app/[id]/summary.tsx
+++ app/[id]/summary.tsx
@@ -1,8 +1,6 @@
-import { HOST } from "@/lib/const";
-
 export async function Summary({ id }: { id: string }) {
-  const res = await fetch(`${HOST}/query?id=${id}`, {
-    cache: "no-cache",
+  const res = await fetch(`${process.env.API_HOST}/query?id=${id}`, {
+    cache: "no-store",
   });
   const sum = await res.text();
   return <p className="font-mono tracking-wider leading-normal">{sum}</p>;
